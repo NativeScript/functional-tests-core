1ce5fb6d6e3c89479c3eda0b801d51efda4cc1ae
dtopuzov <dimitar.topuzov@telerik.com>
2015-11-26 13:34:37 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-mvn install:install-file -Dfile=lib/opencv-2.4.9/opencv-249.jar -DgroupId=opencv -DartifactId=opencv -Dversion=2.4.9 -Dpackaging=jar
-mvn clean compile assembly:single
-rm -rf ../functional-tests-new/lib/functional.tests.core-0.0.1.jar
-cp target/functional.tests.core-0.0.1-jar-with-dependencies.jar ../functional-tests-new/lib/functional.tests.core-0.0.1.jar
-mvn install:install-file -Dfile=../functional-tests-new/lib/functional.tests.core-0.0.1.jar -DgroupId=functional.tests.core -DartifactId=functional.tests.core -Dversion=0.0.1 -Dpackaging=jar
+if [ "$1" == "fast" ]; then
+  echo "Run Fast Build"
+  mvn clean install
+  mvn install:install-file -Dfile=target/functional.tests.core-0.0.1.jar -DgroupId=functional.tests.core -DartifactId=functional.tests.core -Dversion=0.0.1 -Dpackaging=jar
+else
+  echo "Run Full Build"
+  mvn install:install-file -Dfile=lib/opencv-2.4.9/opencv-249.jar -DgroupId=opencv -DartifactId=opencv -Dversion=2.4.9 -Dpackaging=jar
+  mvn clean compile assembly:single
+  rm -rf ../functional-tests-new/lib/functional.tests.core-0.0.1.jar
+  cp target/functional.tests.core-0.0.1-jar-with-dependencies.jar ../functional-tests-new/lib/functional.tests.core-0.0.1.jar
+  mvn install:install-file -Dfile=../functional-tests-new/lib/functional.tests.core-0.0.1.jar -DgroupId=functional.tests.core -DartifactId=functional.tests.core -Dversion=0.0.1 -Dpackaging=jar
+fi
