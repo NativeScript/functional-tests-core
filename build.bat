546f8dd6bbd17d16a5b3aba5cf176e8022be0d86
vchimev <Vasil.Chimev@telerik.com>
2016-07-12 13:25:01 +0300

    Upgrade OpenCV version to 2.4.13

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 IF "%1"=="fast" GOTO FastBuild
-call mvn install:install-file -Dfile=lib/opencv-2.4.9/opencv-249.jar -DgroupId=opencv -DartifactId=opencv -Dversion=2.4.9 -Dpackaging=jar
+call mvn install:install-file -Dfile=lib/opencv-2.4.13/opencv-2413.jar -DgroupId=opencv -DartifactId=opencv -Dversion=2.4.13 -Dpackaging=jar
 call mvn clean compile assembly:single
 call del ..\functional-tests-new\lib\functional.tests.core-0.0.3.jar
 call copy target\functional.tests.core-0.0.3-jar-with-dependencies.jar ..\functional-tests-new\lib\functional.tests.core-0.0.3.jar
--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ if [ "$1" == "fast" ]; then
 
 elif [ "$1" == "deploy" ]; then
   echo "Full Build and Deploy"
-  mvn install:install-file -Dfile=lib/opencv-2.4.9/opencv-249.jar -DgroupId=opencv -DartifactId=opencv -Dversion=2.4.9 -Dpackaging=jar
+  mvn install:install-file -Dfile=lib/opencv-2.4.13/opencv-2413.jar -DgroupId=opencv -DartifactId=opencv -Dversion=2.4.13 -Dpackaging=jar
   mvn clean compile assembly:single
 
   rm -rf ../functional-tests-new/lib/functional.tests.core-0.0.3.jar
@@ -19,7 +19,7 @@ elif [ "$1" == "deploy" ]; then
 
 else
   echo "Full Build"
-  mvn install:install-file -Dfile=lib/opencv-2.4.9/opencv-249.jar -DgroupId=opencv -DartifactId=opencv -Dversion=2.4.9 -Dpackaging=jar
+  mvn install:install-file -Dfile=lib/opencv-2.4.13/opencv-2413.jar -DgroupId=opencv -DartifactId=opencv -Dversion=2.4.13 -Dpackaging=jar
   mvn clean compile assembly:single
   rm -rf target/functional.tests.core-0.0.3.jar
   mv target/functional.tests.core-0.0.3-jar-with-dependencies.jar target/functional.tests.core-0.0.3.jar
