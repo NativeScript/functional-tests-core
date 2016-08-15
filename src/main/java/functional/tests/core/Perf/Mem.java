9bf5d510975b56a40242aed72e2196c8a8eea6d1
Dimitar Topuzov <dimitar.topuzov@telerik.com>
2016-08-15 16:59:39 +0300

    Get memmory of app

--- src/main/java/functional/tests/core/Perf/Mem.java
+++ src/main/java/functional/tests/core/Perf/Mem.java
@@ -5,19 +5,22 @@ import functional.tests.core.Log.Log;
 import functional.tests.core.OSUtils.OSUtils;
 import functional.tests.core.Settings.Settings;
 
-import java.io.File;
+public class PerfInfo {
 
-public class Mem {
-
-    public static void saveRowMemInfo(String fileName) {
+    public static long getMem() {
         if (Settings.platform == PlatformType.Andorid) {
-            String command = "adb -s " + Settings.deviceId +
-                    " shell dumpsys meminfo > " +
-                    Settings.baseLogDir + File.separator + fileName;
-            OSUtils.runProcess(command);
-            Log.info("Meminfo saved in " + fileName);
-        } else if (Settings.platform == PlatformType.iOS) {
-            // TODO: Implement it
+            String command = "adb -s " + Settings.deviceId + " shell dumpsys meminfo  | grep " + Settings.packageId;
+            String output = OSUtils.runProcess(command);
+            if (output.contains(Settings.packageId)) {
+                String memString = output.split(":")[0];
+                memString = memString.replace("kB", "").trim();
+                return Long.parseLong(memString);
+            } else {
+                return 0;
+            }
+        } else {
+            Log.error("Get memory is implemented only for Android.");
+            return 0;
         }
     }
 }
