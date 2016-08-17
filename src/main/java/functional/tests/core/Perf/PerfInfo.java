1962e45a2c551ff1cda42102e02bd0e5449e56c9
Dimitar Topuzov <dimitar.topuzov@telerik.com>
2016-08-17 10:57:07 +0300

    Fix get mem method

--- src/main/java/functional/tests/core/Perf/PerfInfo.java
+++ src/main/java/functional/tests/core/Perf/PerfInfo.java
@@ -1,16 +1,17 @@
 package functional.tests.core.Perf;
 
+import functional.tests.core.Device.Android.Adb;
 import functional.tests.core.Enums.PlatformType;
 import functional.tests.core.Log.Log;
-import functional.tests.core.OSUtils.OSUtils;
 import functional.tests.core.Settings.Settings;
 
 public class PerfInfo {
 
     public static long getMem() {
         if (Settings.platform == PlatformType.Andorid) {
-            String command = "adb -s " + Settings.deviceId + " shell dumpsys meminfo  | grep " + Settings.packageId;
-            String output = OSUtils.runProcess(command);
+            String command = "shell dumpsys meminfo  | grep " + Settings.packageId;
+            String output = Adb.runAdbCommand(Settings.deviceId, command);
+
             if (output.contains(Settings.packageId)) {
                 String memString = output.split(":")[0];
                 memString = memString.replace("kB", "").trim();
