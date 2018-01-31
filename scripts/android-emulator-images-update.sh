158d19b376cc9aa0e289c938b3869a4014e4b59b
Karel Vervaeke <karel.vervaeke@aca-it.be>
2018-01-31 21:21:43 +0100

    Updated scripts to use avdmanager and sdkmanager

--- scripts/android-emulator-images-update.sh
+++ scripts/android-emulator-images-update.sh
@@ -2,46 +2,67 @@
 
 # This script will update
 # Intel HAXM (required for accelerated emulators)
-# Android emulator images from 4.2 (android-17) to 7.1 (android-25)
+# Android emulator images from 4.2 (android-17) to 8.1 (android-27)
 #
 # Notes:
 # This script will work on both Linux and macOS hosts!
 # This script will install both arm and x86 emulators plus google api images!
 # Emulator images require a lot of disk space!
 
+set -e
+set -x
+
 echo "Update Intel HAXM"
-echo y | $ANDROID_HOME/tools/android update sdk --filter extra-intel-Hardware_Accelerated_Execution_Manager --all --no-ui
-sudo $ANDROID_HOME/extras/intel/Hardware_Accelerated_Execution_Manager/silent_install.sh
+echo y | "${ANDROID_HOME}/tools/bin/sdkmanager" "extras;intel;Hardware_Accelerated_Execution_Manager"
+sudo sh "${ANDROID_HOME}/extras/intel/Hardware_Accelerated_Execution_Manager/silent_install.sh"
 
-echo "Update Android Emulator Images"
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-x86_64-google_apis-25 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-x86-google_apis-25 --all --no-ui
+PACKAGES=()
+PACKAGES+=("system-images;android-17;default;armeabi-v7a")
+PACKAGES+=("system-images;android-17;default;mips")
+PACKAGES+=("system-images;android-17;default;x86")
+PACKAGES+=("system-images;android-17;google_apis;armeabi-v7a")
+PACKAGES+=("system-images;android-17;google_apis;x86")
+PACKAGES+=("system-images;android-18;default;armeabi-v7a")
+PACKAGES+=("system-images;android-18;default;x86")
+PACKAGES+=("system-images;android-18;google_apis;armeabi-v7a")
+PACKAGES+=("system-images;android-18;google_apis;x86")
+PACKAGES+=("system-images;android-19;default;armeabi-v7a")
+PACKAGES+=("system-images;android-19;default;x86")
+PACKAGES+=("system-images;android-19;google_apis;armeabi-v7a")
+PACKAGES+=("system-images;android-19;google_apis;x86")
+PACKAGES+=("system-images;android-21;default;armeabi-v7a")
+PACKAGES+=("system-images;android-21;default;x86")
+PACKAGES+=("system-images;android-21;default;x86_64")
+PACKAGES+=("system-images;android-21;google_apis;armeabi-v7a")
+PACKAGES+=("system-images;android-21;google_apis;x86")
+PACKAGES+=("system-images;android-21;google_apis;x86_64")
+PACKAGES+=("system-images;android-22;default;armeabi-v7a")
+PACKAGES+=("system-images;android-22;default;x86")
+PACKAGES+=("system-images;android-22;default;x86_64")
+PACKAGES+=("system-images;android-22;google_apis;armeabi-v7a")
+PACKAGES+=("system-images;android-22;google_apis;x86")
+PACKAGES+=("system-images;android-22;google_apis;x86_64")
+PACKAGES+=("system-images;android-23;default;x86")
+PACKAGES+=("system-images;android-23;default;x86_64")
+PACKAGES+=("system-images;android-23;google_apis;armeabi-v7a")
+PACKAGES+=("system-images;android-23;google_apis;x86")
+PACKAGES+=("system-images;android-23;google_apis;x86_64")
+PACKAGES+=("system-images;android-24;default;arm64-v8a")
+PACKAGES+=("system-images;android-24;default;armeabi-v7a")
+PACKAGES+=("system-images;android-24;default;x86")
+PACKAGES+=("system-images;android-24;default;x86_64")
+PACKAGES+=("system-images;android-24;google_apis;arm64-v8a")
+PACKAGES+=("system-images;android-24;google_apis;armeabi-v7a")
+PACKAGES+=("system-images;android-24;google_apis;x86")
+PACKAGES+=("system-images;android-24;google_apis;x86_64")
+PACKAGES+=("system-images;android-25;google_apis;arm64-v8a")
+PACKAGES+=("system-images;android-25;google_apis;armeabi-v7a")
+PACKAGES+=("system-images;android-25;google_apis;x86")
+PACKAGES+=("system-images;android-25;google_apis;x86_64")
+PACKAGES+=("system-images;android-26;google_apis;x86")
+PACKAGES+=("system-images;android-26;google_apis;x86_64")
+PACKAGES+=("system-images;android-27;google_apis;x86")
 
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-x86-android-24 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-armeabi-v7a-android-24 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-arm64-v8a-android-24 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-x86-google_apis-24 --all --no-ui
+echo "Update Emulator Images"
+yes | "${ANDROID_HOME}/tools/bin/sdkmanager" "${PACKAGES[@]}"
 
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-x86-android-23 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-x86-google_apis-23 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-armeabi-v7a-android-23 --all --no-ui
-
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-x86-android-22 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-x86-google_apis-22 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-armeabi-v7a-android-22 --all --no-ui
-
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-x86-android-21 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-x86-google_apis-21 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-armeabi-v7a-android-21 --all --no-ui
-
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-x86-android-19 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-armeabi-v7a-android-19 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-x86-google_apis-19 --all --no-ui
-
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-x86-android-18 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-armeabi-v7a-android-18 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-x86-google_apis-18 --all --no-ui
-
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-x86-android-17 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-armeabi-v7a-android-17 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter sys-img-x86-google_apis-17 --all --no-ui
--- scripts/android-sdk-update.sh
+++ scripts/android-sdk-update.sh
@@ -2,24 +2,33 @@
 
 # This script will update
 # Android tools, build tools and platform tools
-# Android SDKs from 4.2 (android-17) to 7.1 (android-25)
+# Android SDKs from 4.2 (android-17) to 8.1 (android-27)
 #
 # Notes: This script will work on both Linux and macOS hosts!
 
 set -e
 set -x
 
+PACKAGES=()
+PACKAGES+=("platform-tools")
+PACKAGES+=("tools")
+PACKAGES+=("build-tools;27.0.3")
+PACKAGES+=("extras;android;m2repository")
+PACKAGES+=("extras;google;m2repository")
+PACKAGES+=("platforms;android-7")
+PACKAGES+=("platforms;android-8")
+PACKAGES+=("platforms;android-9")
+PACKAGES+=("platforms;android-27")
+PACKAGES+=("platforms;android-26")
+PACKAGES+=("platforms;android-25")
+PACKAGES+=("platforms;android-24")
+PACKAGES+=("platforms;android-23")
+PACKAGES+=("platforms;android-22")
+PACKAGES+=("platforms;android-21")
+PACKAGES+=("platforms;android-19")
+PACKAGES+=("platforms;android-18")
+PACKAGES+=("platforms;android-17")
+
 echo "Update Android SDK"
-yes | $ANDROID_HOME/tools/bin/sdkmanager platform-tools
-yes | $ANDROID_HOME/tools/bin/sdkmanager tools
-yes | $ANDROID_HOME/tools/bin/sdkmanager 'build-tools;25.0.0'
-yes | $ANDROID_HOME/tools/bin/sdkmanager 'platforms;android-25'
-yes | $ANDROID_HOME/tools/bin/sdkmanager 'platforms;android-24'
-yes | $ANDROID_HOME/tools/bin/sdkmanager 'platforms;android-23'
-yes | $ANDROID_HOME/tools/bin/sdkmanager 'platforms;android-22'
-yes | $ANDROID_HOME/tools/bin/sdkmanager 'platforms;android-21'
-yes | $ANDROID_HOME/tools/bin/sdkmanager 'platforms;android-19'
-yes | $ANDROID_HOME/tools/bin/sdkmanager 'platforms;android-18'
-yes | $ANDROID_HOME/tools/bin/sdkmanager 'platforms;android-17'
-yes | $ANDROID_HOME/tools/bin/sdkmanager 'extras;android;m2repository'
-yes | $ANDROID_HOME/tools/bin/sdkmanager 'extras;google;m2repository'
+yes | "${ANDROID_HOME}/tools/bin/sdkmanager" "${PACKAGES[@]}"
+
