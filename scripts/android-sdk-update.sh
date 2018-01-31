ba8bb6a0212876db01938ec2069cf0b67a311d31
Karel Vervaeke <karel.vervaeke@aca-it.be>
2018-01-31 11:15:07 +0100

    Updated android-sdk-update script

--- scripts/android-sdk-update.sh
+++ scripts/android-sdk-update.sh
@@ -6,17 +6,20 @@
 #
 # Notes: This script will work on both Linux and macOS hosts!
 
+set -e
+set -x
+
 echo "Update Android SDK"
-echo y | $ANDROID_HOME/tools/android update sdk --filter platform-tools --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter tools --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter build-tools-25.0.0 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter android-25 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter android-24 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter android-23 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter android-22 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter android-21 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter android-19 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter android-18 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter android-17 --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter extra-android-m2repository --all --no-ui
-echo y | $ANDROID_HOME/tools/android update sdk --filter extra-google-m2repository --all --no-ui
+yes | $ANDROID_HOME/tools/bin/sdkmanager platform-tools
+yes | $ANDROID_HOME/tools/bin/sdkmanager tools
+yes | $ANDROID_HOME/tools/bin/sdkmanager 'build-tools;25.0.0'
+yes | $ANDROID_HOME/tools/bin/sdkmanager 'platforms;android-25'
+yes | $ANDROID_HOME/tools/bin/sdkmanager 'platforms;android-24'
+yes | $ANDROID_HOME/tools/bin/sdkmanager 'platforms;android-23'
+yes | $ANDROID_HOME/tools/bin/sdkmanager 'platforms;android-22'
+yes | $ANDROID_HOME/tools/bin/sdkmanager 'platforms;android-21'
+yes | $ANDROID_HOME/tools/bin/sdkmanager 'platforms;android-19'
+yes | $ANDROID_HOME/tools/bin/sdkmanager 'platforms;android-18'
+yes | $ANDROID_HOME/tools/bin/sdkmanager 'platforms;android-17'
+yes | $ANDROID_HOME/tools/bin/sdkmanager 'extras;android;m2repository'
+yes | $ANDROID_HOME/tools/bin/sdkmanager 'extras;google;m2repository'
