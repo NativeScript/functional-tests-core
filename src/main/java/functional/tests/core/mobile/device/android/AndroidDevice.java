5bece94bc72f409163b21e06fd364ddcbd59aac4
Dimitar Topuzov <dtopuzov@gmail.com>
2018-07-18 00:21:32 -0700

    fix: support api28

--- src/main/java/functional/tests/core/mobile/device/android/AndroidDevice.java
+++ src/main/java/functional/tests/core/mobile/device/android/AndroidDevice.java
@@ -92,6 +92,7 @@ public class AndroidDevice implements IDevice {
     private static HashMap<Double, Double> newEmulatorMap() {
         HashMap<Double, Double> newEmulatorMap = new HashMap<>();
         newEmulatorMap.put(27.0, 8.1);
+        newEmulatorMap.put(28.0, 9.1);
         return newEmulatorMap;
     }
 
