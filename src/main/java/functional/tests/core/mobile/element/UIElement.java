7d51efabfcf74f87e53514e7ff276a76f424bc15
Dimitar Topuzov <dtopuzov@gmail.com>
2018-09-12 01:08:47 -0700

    fix: remove unused import

--- src/main/java/functional/tests/core/mobile/element/UIElement.java
+++ src/main/java/functional/tests/core/mobile/element/UIElement.java
@@ -14,7 +14,6 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.MultiTouchAction;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.ios.IOSElement;
 import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.ElementOption;
 import io.appium.java_client.touch.offset.PointOption;
